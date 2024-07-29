daca1ed1f3c338f3cd6362b765fa1a86499bc90d
khnarham <khnarham5678@gmail.com>
2024-07-29 19:30:03 +0500

    first commit

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const protectedRoute = createRouteMatcher([
+        '/events/:id',
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        '/api/uploadthing',
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        '/api/uploadthing'
+
+]);
+
+export default clerkMiddleware((auth, req) => {
+    if (protectedRoute(req)) auth().protect();
+  });
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
